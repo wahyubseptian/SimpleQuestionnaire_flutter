b18fe9a769ea5d732f62bb7df6a9a073be03b583
wahyubseptian <wahyubseptian@gmail.com>
2022-02-28 21:12:59 +0700

    first initial

---
+++ lib/answer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(4),
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
